582516c61885fd5f110e201bde77b2a2b113c4c0
FacelessLake <sinonim147@gmail.com>
2023-05-30 21:02:52 +0700

    Shedules added
    Work in progress

--- DB_tasks/D6/main.py
+++ DB_tasks/D6/main.py
@@ -56,9 +56,11 @@ class CityAirports(Resource):
     def get(self, city):
         cursor = get_db().cursor()
         cursor.execute(
-            """SELECT airport_name, airport_code, timezone FROM airports_data WHERE city->>'en' = %s OR city->>'ru' = %s ORDER BY airport_name""",
+            """SELECT airport_name, airport_code, timezone FROM airports_data 
+            WHERE city->>'en' = %s OR city->>'ru' = %s ORDER BY airport_name""",
             [city, city])
         airports = cursor.fetchall()
+        cursor.close()
         result = list(map(lambda a: {
             "name": a[0],
             "abbreviate": a[1],
@@ -69,7 +71,47 @@ class CityAirports(Resource):
         return result
 
 
+class InboundSchedule(Resource):
+    def get(self, airport):
+        cursor = get_db().cursor()
+        cursor.execute(
+            """SELECT flight_no, departure_airport, scheduled_arrival, actual_arrival, arrival_airport FROM flights 
+           WHERE arrival_airport = %s ORDER BY scheduled_arrival""", [airport])
+        inbound_schedule = cursor.fetchall()
+        cursor.close()
+
+        result = list(map(lambda a: {
+            "flight_no": a[0],
+            "origin": a[1],
+            "arrival_date": (a[2] if a[3] is None else a[3]).strftime('%Y-%m-%d %H:%M:%S %z')
+        }, inbound_schedule))
+        if len(result) == 0:
+            return make_response(jsonify({'error': 'Airports not found'}), 404)
+        return result
+
+
+class OutboundSchedule(Resource):
+    def get(self, airport):
+        cursor = get_db().cursor()
+        cursor.execute(
+            """SELECT flight_no, arrival_airport, scheduled_departure, actual_departure, departure_airport FROM flights 
+           WHERE departure_airport = %s ORDER BY scheduled_arrival""", [airport])
+        inbound_schedule = cursor.fetchall()
+        cursor.close()
+
+        result = list(map(lambda a: {
+            "flight_no": a[0],
+            "destination": a[1],
+            "departure_date": (a[2] if a[3] is None else a[3]).strftime('%Y-%m-%d %H:%M:%S %z')
+        }, inbound_schedule))
+        if len(result) == 0:
+            return make_response(jsonify({'error': 'Airports not found'}), 404)
+        return result
+
+
 api.add_resource(Cities, '/cities')
 api.add_resource(Airports, '/airports')
 api.add_resource(CityAirports, '/airports/<city>')
+api.add_resource(InboundSchedule, '/airports/<airport>/inbound')
+api.add_resource(OutboundSchedule, '/airports/<airport>/outbound')
 app.run(debug=True)
